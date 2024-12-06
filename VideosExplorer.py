c3df84ebb7e4f936fc0f5e5be257b9a781461bb2
Михаил Шелудченко <theoutstandingme@gmail.com>
2024-12-06 22:27:39 +0700

    stores videos in mongo

--- VideosExplorer.py
+++ VideosExplorer.py
@@ -1,13 +1,13 @@
 import json
 from time import sleep
-
+import pymongo
 import requests
 import datetime
+
+from pymongo.errors import DuplicateKeyError
 from selenium import webdriver
 from selenium.common import NoSuchElementException, StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 client_id = 52812873 # application id
@@ -34,17 +34,18 @@ video_list_response = requests.get(
 video_base_url = 'https://vkvideo.ru/video'
 videos = [
     {
+        'id': item['id'],
         'title': item['title'],
         'page_url': f'{video_base_url}{item['owner_id']}_{item['id']}',
         'date': datetime.datetime.utcfromtimestamp(float(item['date']))
     } for item in json.loads(video_list_response.text)['response']['items']
-][0:4]
+][0:7]
 
 browser = webdriver.Chrome()
 for video in videos:
     try:
         browser.get(video['page_url'])
-        sleep(10)
+        sleep(7)
         # WebDriverWait(browser, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,'[class*="vkitTwoColumnLayoutMain__root"] [class*="VideoPage__player"] + .vkuiDiv.vkuiRootComponent')))
         video_details_wrapper = browser.find_element(By.CSS_SELECTOR, '[class*="vkitTwoColumnLayoutMain__root"] [class*="VideoPage__player"] + .vkuiDiv.vkuiRootComponent')
         try:
@@ -71,4 +72,15 @@ for video in videos:
         sleep(3)
     except TimeoutException as e:
         pass
-print(videos)
+
+mongo_client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+db = mongo_client.get_database('vk_videos')
+is_collection_new = query not in db.list_collection_names()
+collection = db.get_collection(query)
+if is_collection_new:
+    collection.create_index('id', unique=True)
+for video in videos:
+    try:
+        collection.insert_one(video)
+    except DuplicateKeyError as e:
+        pass
